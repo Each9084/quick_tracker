ee287c5e27c2abae5d8da3eccfe2c5151a5c478a
Each9084 <each9084@gmail.com>
2025-02-18 23:27:26 +0000

    change the stateless to stateful

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -4,9 +4,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:quick_tracker/screens/home/views/main_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../../states/states_screen.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  var widgetList = [
+    MainScreen(),
+    stateScreen(),
+  ];
+
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +31,12 @@ class HomeScreen extends StatelessWidget {
       bottomNavigationBar: ClipRRect(
         borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
         child: BottomNavigationBar(
+          onTap: (value) {
+           setState(() {
+             index = value;
+           });
+            // print(value);
+          },
           backgroundColor: Colors.white,
           showSelectedLabels: false,
           showUnselectedLabels: false,
@@ -35,24 +55,24 @@ class HomeScreen extends StatelessWidget {
         onPressed: () {},
         shape: CircleBorder(),
         child: Container(
-          width: 60,
+            width: 60,
             height: 60,
-          //添加渐变
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            gradient: LinearGradient(colors: [
-              Theme.of(context).colorScheme.primary,
-              Theme.of(context).colorScheme.secondary,
-              Theme.of(context).colorScheme.tertiary,
-            ],
-            transform: GradientRotation(pi/4))
-          ),
+            //添加渐变
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(colors: [
+                  Theme.of(context).colorScheme.primary,
+                  Theme.of(context).colorScheme.secondary,
+                  Theme.of(context).colorScheme.tertiary,
+                ], transform: GradientRotation(pi / 4))),
             child: Icon(
-          CupertinoIcons.add,
-          color: Colors.white,
-        )),
+              CupertinoIcons.add,
+              color: Colors.white,
+            )),
       ),
-      body: MainScreen(),
+      //如果为0则是 主页,不是则是状态页
+      body: index == 0 ? MainScreen() : stateScreen(),
     );
+    ;
   }
 }
