3da39ac7462880b72a624c2c85865d732d3c30ba
Each9084 <each9084@gmail.com>
2025-02-18 21:06:36 +0000

    change the name

---
+++ lib/screens/states/states_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class stateScreen extends StatelessWidget {
+  const stateScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
