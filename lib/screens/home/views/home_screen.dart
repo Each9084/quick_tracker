2536f1333738194a880884258bf1f61584423bda
Each9084 <each9084@gmail.com>
2025-02-19 22:15:35 +0000

    add selectedColor to switch the outline in bottomBar

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quick_tracker/screens/add_expense/views/add_expense.dart';
 import 'package:quick_tracker/screens/home/views/main_screen.dart';
 
 import '../../states/states_screen.dart';
@@ -14,15 +15,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  //影响有多少个item
+  int index = 0;
+
+  //当切换到不同的页面 在bar中切换颜色
+  late Color selectedColor;
+  Color unSelectedColor = Colors.grey;
 
   var widgetList = [
     MainScreen(),
     stateScreen(),
   ];
 
-  int index = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+  }
+
   @override
   Widget build(BuildContext context) {
+    selectedColor = Theme.of(context).colorScheme.primary;
+
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 10,
@@ -32,27 +45,45 @@ class _HomeScreenState extends State<HomeScreen> {
         borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
         child: BottomNavigationBar(
           onTap: (value) {
-           setState(() {
-             index = value;
-           });
+            setState(() {
+              index = value;
+            });
             // print(value);
           },
+          //fixedColor: Colors.green,
+          //被selectedColor取代了 selectedItemColor: Colors.green,
           backgroundColor: Colors.white,
           showSelectedLabels: false,
           showUnselectedLabels: false,
           elevation: 3,
           items: [
             BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.home), label: "Home"),
+              icon: Icon(
+                CupertinoIcons.home,
+                color: index == 0 ? selectedColor : unSelectedColor,
+              ),
+              label: "Home",
+            ),
             BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.graph_square_fill), label: "Stats"),
+                icon: Icon(
+                  CupertinoIcons.graph_square_fill,
+                  color: index == 0 ? unSelectedColor : selectedColor,
+                ),
+                label: "Stats"),
           ],
         ),
       ),
       //浮空栏 用于添加
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) => AddExpense(),
+            ),
+          );
+        },
         shape: CircleBorder(),
         child: Container(
             width: 60,
