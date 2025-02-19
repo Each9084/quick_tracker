146edeea0276c6ee4b2adae7515b91f1b3fd7505
Each9084 <each9084@gmail.com>
2025-02-19 22:14:11 +0000

    Adding Constraints

--- lib/screens/states/states_screen.dart
+++ lib/screens/states/states_screen.dart
@@ -1,4 +1,6 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:quick_tracker/screens/states/chart.dart';
 
 class stateScreen extends StatelessWidget {
   const stateScreen({super.key});
@@ -9,13 +11,31 @@ class stateScreen extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Transaction",style: TextStyle(
-              color: Colors.black,
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
-            ),)
-
+            Text(
+              "Transaction",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: MyChart(),
+              ),
+            )
           ],
         ),
       ),
