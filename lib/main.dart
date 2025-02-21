0a47bf7a2710599d5e645f0d4f6cb101535469da
Each9084 <each9084@gmail.com>
2025-02-21 21:02:38 +0000

    lack of the SimpleBlocObserver

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'app.dart';
+import 'package:equatable/equatable.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+ WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp();
+ Bloc.observer = SimpleBlocObserver();
+ runApp(const MyApp());
 }
 
